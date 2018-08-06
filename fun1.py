3348680f9f5c6e1ffc1e7fb3a10d2060cd357c3b
mansour20-meet <mansour20@meet.mit.edu>
2018-08-06 07:31:16 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+def add_numbers(int1,5000)):
+    """
+    write the body of this
+    function, similar to the block
+    of code we just saw. Hint:
+    don’t forget to use return
+    """
+
+test1 = add_numbers(1,2)
+print(test1)
+test2 = add_numbers(1,100)
+print(test2)
+test3 = add_numbers(1000,5000)
+print(test3)
+
